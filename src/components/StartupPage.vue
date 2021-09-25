e55618a1aca8216f84a71875793221a5e6625088
Henk <lozynskiwojciech@gmail.com>
2021-09-26 00:58:31 +0200

    initial commit

---
+++ src/components/StartupPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="q-pa-md q-gutter-md">
+    <div class="row justify-between">
+
+      <q-parallax
+          src="https://media.wired.com/photos/5a723fcbf838923d23e1b4ff/16:9/w_2400,h_1350,c_limit/archives-488979131-%5BConverted%5D.jpg"
+      >
+        <h1 class="text-white">Basic</h1>
+      </q-parallax>
+
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
